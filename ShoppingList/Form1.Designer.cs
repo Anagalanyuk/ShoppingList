edd9db73737371a19017420a617128f493339b51
Anatoliy Anatoliy <anagalanyuk@gmail.com>
2018-06-15 14:12:28 +0300

    version 1

--- ShoppingList/Form1.Designer.cs
+++ ShoppingList/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ShoppingList
 {
-	partial class Form1
+	partial class MainForm
 	{
 		/// <summary>
 		/// Required designer variable.
@@ -28,13 +28,109 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.nameProduct = new System.Windows.Forms.TextBox();
+			this.addButton = new System.Windows.Forms.Button();
+			this.upButton = new System.Windows.Forms.Button();
+			this.downButton = new System.Windows.Forms.Button();
+			this.deleteButton = new System.Windows.Forms.Button();
+			this.listProducts = new System.Windows.Forms.CheckedListBox();
+			this.SuspendLayout();
+			// 
+			// nameProduct
+			// 
+			this.nameProduct.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.nameProduct.Location = new System.Drawing.Point(12, 307);
+			this.nameProduct.Name = "nameProduct";
+			this.nameProduct.Size = new System.Drawing.Size(214, 20);
+			this.nameProduct.TabIndex = 1;
+			this.nameProduct.TextChanged += new System.EventHandler(this.nameProduct_TextChanged);
+			// 
+			// addButton
+			// 
+			this.addButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.addButton.Location = new System.Drawing.Point(232, 305);
+			this.addButton.Name = "addButton";
+			this.addButton.Size = new System.Drawing.Size(75, 23);
+			this.addButton.TabIndex = 2;
+			this.addButton.Text = "Add";
+			this.addButton.UseVisualStyleBackColor = true;
+			this.addButton.Click += new System.EventHandler(this.addButton_Click);
+			// 
+			// upButton
+			// 
+			this.upButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.upButton.Enabled = false;
+			this.upButton.Location = new System.Drawing.Point(324, 305);
+			this.upButton.Name = "upButton";
+			this.upButton.Size = new System.Drawing.Size(75, 23);
+			this.upButton.TabIndex = 3;
+			this.upButton.Text = "Up";
+			this.upButton.UseVisualStyleBackColor = true;
+			// 
+			// downButton
+			// 
+			this.downButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.downButton.Enabled = false;
+			this.downButton.Location = new System.Drawing.Point(324, 333);
+			this.downButton.Name = "downButton";
+			this.downButton.Size = new System.Drawing.Size(75, 23);
+			this.downButton.TabIndex = 4;
+			this.downButton.Text = "Down";
+			this.downButton.UseVisualStyleBackColor = true;
+			// 
+			// deleteButton
+			// 
+			this.deleteButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.deleteButton.Location = new System.Drawing.Point(12, 333);
+			this.deleteButton.Name = "deleteButton";
+			this.deleteButton.Size = new System.Drawing.Size(295, 23);
+			this.deleteButton.TabIndex = 5;
+			this.deleteButton.Text = "Delete";
+			this.deleteButton.UseVisualStyleBackColor = true;
+			this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+			// 
+			// listProducts
+			// 
+			this.listProducts.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.listProducts.FormattingEnabled = true;
+			this.listProducts.Location = new System.Drawing.Point(12, 11);
+			this.listProducts.Name = "listProducts";
+			this.listProducts.Size = new System.Drawing.Size(394, 289);
+			this.listProducts.TabIndex = 7;
+			this.listProducts.SelectedIndexChanged += new System.EventHandler(this.listProducts_SelectedIndexChanged);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(411, 368);
+			this.Controls.Add(this.listProducts);
+			this.Controls.Add(this.deleteButton);
+			this.Controls.Add(this.downButton);
+			this.Controls.Add(this.upButton);
+			this.Controls.Add(this.addButton);
+			this.Controls.Add(this.nameProduct);
+			this.KeyPreview = true;
+			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "Form1";
+			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainForm_KeyDown);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+		private System.Windows.Forms.TextBox nameProduct;
+		private System.Windows.Forms.Button addButton;
+		private System.Windows.Forms.Button upButton;
+		private System.Windows.Forms.Button downButton;
+		private System.Windows.Forms.Button deleteButton;
+		private System.Windows.Forms.CheckedListBox listProducts;
 	}
 }
 
--- ShoppingList/Form1.cs
+++ ShoppingList/Form1.cs
@@ -1,20 +1,100 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ShoppingList
 {
-	public partial class Form1 : Form
+	public partial class MainForm : Form
 	{
-		public Form1()
+		public MainForm()
 		{
 			InitializeComponent();
+			addButton.Enabled = false;
+			deleteButton.Enabled = false;
+			//downButton.Enabled = false;
+			//upButton.Enabled = false;
+		}
+
+		private void Add()
+		{
+			nameProduct.Text = nameProduct.Text.Trim();
+			listProducts.Items.Add(nameProduct.Text);
+			nameProduct.Clear();
+		}
+		private void addButton_Click(object sender, EventArgs e)
+		{
+			Add();
+		}
+
+		private void deleteButton_Click(object sender, EventArgs e)
+		{
+			if (listProducts.SelectedIndex >= 0)
+			{
+				listProducts.Items.RemoveAt(listProducts.SelectedIndex);
+			}
+		}
+
+		private void MainForm_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Enter)
+			{
+				if (addButton.Enabled)
+				{
+					Add();
+				}
+			}
+		}
+
+		private void nameProduct_TextChanged(object sender, EventArgs e)
+		{
+			newProduct();
+
+		}
+
+		private void newProduct()
+		{
+			for (int i = 0; i < nameProduct.Text.Length; ++i)
+			{
+				if (nameProduct.Text[i] != ' ')
+				{
+					addButton.Enabled = true;
+				}
+			}
+			string clone = nameProduct.Text;
+			foreach (string product in listProducts.Items)
+			{
+				clone = nameProduct.Text.Trim();
+				if (product.Equals(clone))
+				{
+					addButton.Enabled = false;
+				}
+			}
+			if (nameProduct.Text.Length == 0)
+			{
+				addButton.Enabled = false;
+			}
+		}
+
+		private void listProducts_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			int index = listProducts.SelectedIndex;
+			if (listProducts.SelectedIndex > 0)
+			{
+				upButton.Enabled = true;
+			}
+			else
+			{
+				upButton.Enabled = false;
+			}
+			if (listProducts.SelectedIndex < listProducts.Items.Count - 1)
+			{
+				downButton.Enabled = true;
+			}
+			else
+			{
+				downButton.Enabled = false;
+			}
+			deleteButton.Enabled = true;
+			newProduct();
 		}
 	}
 }
